8abaaad74a4aa5b179d0c272c67e09456608ae66
Andrea Bissoli <abisso2003@gmail.com>
2025-05-12 23:25:41 +0200

    Aggiunto bcrypt per verifica password con hash e salt

--- app/authentication.js
+++ app/authentication.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
 
 router.post('', async function(req, res) {
 
+    email = req.body.email;
+    password = req.body.password;
+
 	var user = {
-        email: req.body.email,
-        password: req.body.password,
-        id: 1234
     };
 
     // find the user in the local db
@@ -15,20 +17,28 @@ router.post('', async function(req, res) {
     // local user not found
 
     // check if password matches
+    const match = await bcrypt.compare(password, user.hash);
+    if (match) {
+        // password matches
+    }
+    else {
+        // password does not match
+        return res.status(401).json({
+            error: 'Invalid password',
+            errorCode: 'INVALID_PASSWORD'
+        });
+    }
 	
 	// if user is found or created create a token
 	var payload = {
 		email: user.email,
-		id: user.id
-		// other data encrypted in the token	
+		id: user._id
 	}
 	var options = {
 		expiresIn: 86400 // expires in 24 hours
 	}
 	var token = jwt.sign(payload, process.env.JWT_SECRET, options);
 
-    console.log(user);
-    console.log(token);
 	res.json({
 		success: true,
 		token: token,
