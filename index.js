8732bff203081c4dba32afbe693ab707a267f5ba
Andrea Bissoli <abisso2003@gmail.com>
2025-05-13 11:45:43 +0200

    Setup base per connessione al database

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.locals.db = mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
