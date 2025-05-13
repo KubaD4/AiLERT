236d9291ada08a5a695d06b03e3f2c867b0c1b50
Andrea Bissoli <abisso2003@gmail.com>
2025-05-13 18:12:18 +0200

    Frontend con routing

--- Frontend/src/components/AlertMessage.vue
+++ Frontend/src/components/AlertMessage.vue
@@ -12,23 +12,50 @@ defineProps({
 });
 
 const alertClasses = {
-  'info': 'alert-info bg-info/10 text-info border-info/20',
-  'success': 'alert-success bg-success/10 text-success border-success/20',
-  'warning': 'alert-warning bg-warning/10 text-warning border-warning/20',
-  'error': 'alert-error bg-error/10 text-error border-error/20'
+  'info': 'bg-blue-50 text-blue-700 border-blue-200',
+  'success': 'bg-green-50 text-green-700 border-green-200',
+  'warning': 'bg-amber-50 text-amber-700 border-amber-200',
+  'error': 'bg-red-50 text-red-700 border-red-200'
 };
 
-const icons = {
-  'info': `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>`,
-  'success': `<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-5 w-5" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`,
-  'warning': `<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-5 w-5" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>`,
-  'error': `<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-5 w-5" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`
+const iconColors = {
+  'info': 'text-blue-400',
+  'success': 'text-green-400',
+  'warning': 'text-amber-400',
+  'error': 'text-red-400'
 };
 </script>
 
 <template>
-  <div class="alert rounded-md border text-sm py-3" :class="alertClasses[type]">
-    <div v-html="icons[type]"></div>
-    <span>{{ message }}</span>
+  <div class="rounded-lg p-4 border flex items-start space-x-3" :class="alertClasses[type]">
+    <!-- Info icon -->
+    <div v-if="type === 'info'" :class="iconColors[type]">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+      </svg>
+    </div>
+    
+    <!-- Success icon -->
+    <div v-else-if="type === 'success'" :class="iconColors[type]">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+      </svg>
+    </div>
+    
+    <!-- Warning icon -->
+    <div v-else-if="type === 'warning'" :class="iconColors[type]">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+      </svg>
+    </div>
+    
+    <!-- Error icon -->
+    <div v-else :class="iconColors[type]">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+      </svg>
+    </div>
+    
+    <div class="text-sm font-medium">{{ message }}</div>
   </div>
 </template>
---
+++ Frontend/src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import LoginForm from '../components/LoginForm.vue';
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-[80vh] py-12 px-4">
+    <LoginForm />
+  </div>
+</template>
