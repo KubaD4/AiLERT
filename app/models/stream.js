19ed10f055d618edd86362909ee5cb512285ec7c
KubaD4 <kbd4@ymail.com>
2025-05-16 00:12:04 +0200

    Bozza api, schema stream

---
+++ app/models/stream.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const streamSchema = new mongoose.Schema({
+    cameraId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Camera',  // Riferito al modello Camera
+        required: true
+    },
+    streamKey: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    startTime: {
+        type: Date,
+        default: Date.now
+    },
+    endTime: {
+        type: Date
+    },
+    streamUrl: {
+        type: String,
+        required: true
+    },
+    streamType: {
+        type: String,
+        enum: ['rtsp', 'hls', 'webrtc'],
+        default: 'rtsp' //tipo usato per le telecamere a Trento
+    },
+    viewCount: {
+        type: Number,
+        default: 0
+    },
+    lastAccessed: {
+        type: Date
+    }
+});
+
+const Stream = mongoose.model('Stream', streamSchema);
+
+module.exports = Stream;
