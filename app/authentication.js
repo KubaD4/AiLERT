25c9dfe4a2468824f18cbb2ecc4d159504be1c71
Andrea Bissoli <abisso2003@gmail.com>
2025-05-16 12:49:19 +0200

    Aggiunto file di configurazione per variabili globali

--- app/authentication.js
+++ app/authentication.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const SALT_ROUNDS = 10;
+const SALT_ROUNDS = require('./config').SALT_ROUNDS;
 const User = require('./models/user');
 
 router.post('', async function(req, res) {
