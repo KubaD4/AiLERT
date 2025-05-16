464f9bc15d816eb283ce3adcb04047c77cb723bf
KubaD4 <kbd4@ymail.com>
2025-05-16 15:47:19 +0200

    Frontend per vista Telecamere

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+import CamerasView from '../views/CamerasView.vue'
+import CameraDetailView from '../views/CameraDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,6 +21,18 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: () => import('../views/DashboardView.vue')
+    },
+    {
+      path: '/cameras',
+      name: 'cameras',
+      component: CamerasView,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/cameras/:id',
+      name: 'camera-detail',
+      component: CameraDetailView,
+      meta: { requiresAuth: true }
     }
   ]
 })
